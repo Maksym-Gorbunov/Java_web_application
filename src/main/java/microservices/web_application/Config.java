5df4484559ba8dab8a1c48473e65faa479569ada
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-03-17 12:05:31 +0100

    release 3.2

--- src/main/java/microservices/web_application/Config.java
+++ src/main/java/microservices/web_application/Config.java
@@ -3,6 +3,9 @@ package microservices.web_application;
 //Helping config to handle url, easy to change port
 //For another port change here and in application.properties
 public class Config {
-    public static String port = "http://172.19.117.145:3000/";
-    public static String restUrl = "http://172.19.117.145:4000/";
+//    public static String port = "http://172.19.117.145:3000/";
+//    public static String restUrl = "http://172.19.117.145:4000/";
+
+    public static String port = "http://localhost:3000/";
+    public static String restUrl = "http://localhost:4000/";
 }
